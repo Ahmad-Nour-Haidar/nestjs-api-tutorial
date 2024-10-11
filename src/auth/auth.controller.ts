ac11e37518d5e2c9ba0d661afdfb39cea27e3c10
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-11 16:56:15 +0300

    ✨ Add some logic in auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  signin() {
+    return this.authService.signin();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return 'User signed up successfully.';
+  }
+
+  signin() {
+    return 'User signed in successfully.';
+  }
+}
